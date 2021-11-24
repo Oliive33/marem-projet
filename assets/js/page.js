99c64fd1d43462ef69d901c33e6c231b42173e3d
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-11-24 08:34:11 +0100

    ajout page js

---
+++ assets/js/page.js
@@ -0,0 +1,13 @@
+// =================Btn Toggle====================//
+// const btnMenu = document.querySelector(".header__contact");
+// const menu = document.querySelector(".nav-logo");
+
+// btnMenu.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+// });
+
+// const allLinks = document.querySelector(".contact-slogan");
+
+// allLinks.addEventListener("click", () => {
+//   menu.classList.remove("active");
+// });
