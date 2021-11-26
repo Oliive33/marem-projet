a139ccbfc6c87f62c9014179223cff3e9515c8d1
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-11-26 13:22:28 +0100

    ajout slider img

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,57 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+// Slider portfolio
+const sliderPortfolio = document.querySelector(".slider-portfolio");
+const srcEnCoursSlider = document.querySelector(".img-visible-slider");
+const allPicsPortfolio = Array.from(
+  document.querySelectorAll(".grid-item img")
+);
+const rightPorfolio = document.querySelector(".btn-right");
+const leftPorfolio = document.querySelector(".btn-left");
+const fermerSlide = document.querySelector(".btn-fermer-slider");
+
+let photoEnCours;
+let indexEnCours;
+
+allPicsPortfolio.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    sliderPortfolio.style.display = "block";
+    srcEnCoursSlider.src = e.target.src;
+    photoEnCours = e.target;
+    indexEnCours = allPicsPortfolio.indexOf(photoEnCours);
+    console.log(indexEnCours);
+  });
+});
+rightPorfolio.addEventListener("click", () => {
+  if (indexEnCours === 11) {
+    indexEnCours = 0;
+    srcEnCoursSlider.src = allPicsPortfolio[indexEnCours].src;
+    photoEnCours = allPicsPortfolio[indexEnCours];
+    console.log(indexEnCours);
+    return;
+  }
+
+  srcEnCoursSlider.src = allPicsPortfolio[indexEnCours + 1].src;
+  photoEnCours = allPicsPortfolio[indexEnCours + 1];
+  indexEnCours = allPicsPortfolio.indexOf(photoEnCours);
+  console.log(indexEnCours);
+});
+leftPorfolio.addEventListener("click", () => {
+  if (indexEnCours === 0) {
+    indexEnCours = 11;
+    srcEnCoursSlider.src = allPicsPortfolio[indexEnCours].src;
+    photoEnCours = allPicsPortfolio[indexEnCours];
+    console.log(indexEnCours);
+    return;
+  }
+
+  srcEnCoursSlider.src = allPicsPortfolio[indexEnCours - 1].src;
+  photoEnCours = allPicsPortfolio[indexEnCours - 1];
+  indexEnCours = allPicsPortfolio.indexOf(photoEnCours);
+  console.log(indexEnCours);
+});
+
+fermerSlide.addEventListener("click", () => {
+  sliderPortfolio.style.display = "none";
+});
